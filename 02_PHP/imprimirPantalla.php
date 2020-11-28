e2be1e269520fe7138ad9b5a60e090857b9001db
Juan P. Romano <programming@jpromano.net>
2020-11-28 18:20:20 -0300

    Listado de videojuegos

---
+++ 02_PHP/imprimirPantalla.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Imprimir por pantalla en PHP</title>
+</head>
+<body>
+    <?php
+        echo "<h2>Listado de videojuegos</h2>"
+    ?>
+</body>
+</html>
