2b7f780edaebb704cd8e9ec2ad4a45122ef9eb1f
jpromanonet <programming@jpromano.net>
2020-12-01 21:40:01 -0300

    Variables

--- 02_PHP/variables.php
+++ 02_PHP/variables.php
@@ -1,5 +1,6 @@
 <?php
 
+// Una variable es un contenedor de informacion, puedo guardar cualquier dato en el.
 $miPrimeraVariable = "Hola mundo desde PHP";
 
 echo '<h1>'.$miPrimeraVariable.'</h1>';
