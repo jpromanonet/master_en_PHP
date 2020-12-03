431be0d77799f3ced98cee6cb2f85ab24e85fd88
jpromanonet <programming@jpromano.net>
2020-12-03 20:34:21 -0300

    Mas variables

--- 02_PHP/masVariables.php
+++ 02_PHP/masVariables.php
@@ -5,7 +5,8 @@ $miPrimeraVariable = "Hola mundo desde PHP";
 $numero = 44;
 $verdadero = true;
 
-// Le re-asignamos otro valor a numero
+// Le re-asignamos otro valor a numero y mostrara 77 y no 44
+// porque el contenido de las variables se sustituye
 $numero = 77;
 
 echo '<h1>'.$miPrimeraVariable.'</h1>';
