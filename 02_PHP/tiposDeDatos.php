8350f0a97fa0d6c688618129b3e443ce630aa870
jpromanonet <programming@jpromano.net>
2020-12-04 23:03:19 -0300

    Tipos de datos

--- 02_PHP/tiposDeDatos.php
+++ 02_PHP/tiposDeDatos.php
@@ -8,7 +8,7 @@ Coma flotante / decimales (float / double) = 3.45
 Cadenas(strong) = "Hola, yo soy un string"
 Booleano (bool) = true OR false
 null
-
+Array(coleccion o lista de datos indexados)
 */
 
 ?>
