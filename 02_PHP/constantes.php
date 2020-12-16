30b1aa9cff8c6ed897bb184bb7f78134ec3bae33
jpromanonet <programming@jpromano.net>
2020-12-15 21:11:07 -0300

    Constantes

--- 02_PHP/constantes.php
+++ 02_PHP/constantes.php
@@ -3,4 +3,9 @@
     // Las constantes son como las variables PERO 
     // el valor de la constante no puede variar
 
+    define('nombre', 'Juan');
+    define('web', 'jpromano.net');
+
+    echo'<h1>'.nombre.'</h1>';
+
 ?>
