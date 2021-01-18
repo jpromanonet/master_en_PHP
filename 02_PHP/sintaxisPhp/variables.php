99a2b0a20e7586f677ca1483f8f60fbf0be64dc4
Juan P. Romano <programming@jpromano.net>
2021-01-18 11:45:11 -0300

    New dir order

---
+++ 02_PHP/sintaxisPhp/variables.php
@@ -0,0 +1,12 @@
+<?php
+
+// Una variable es un contenedor de informacion, puedo guardar cualquier dato en el.
+$miPrimeraVariable = "Hola mundo desde PHP";
+$numero = 44;
+$verdadero = true;
+
+echo '<h1>'.$miPrimeraVariable.'</h1>';
+echo $numero.'</br>';
+echo $verdadero.'</br>';
+
+?>
