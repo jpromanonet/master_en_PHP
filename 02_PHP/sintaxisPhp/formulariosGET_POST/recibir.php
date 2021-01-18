dba5887eb69cf32484d7a9317b4906538c151656
Juan P. Romano <programming@jpromano.net>
2021-01-18 18:36:14 -0300

    Formulario POST

--- 02_PHP/sintaxisPhp/formulariosGET_POST/recibir.php
+++ 02_PHP/sintaxisPhp/formulariosGET_POST/recibir.php
@@ -1,10 +1,19 @@
 <?php
 
+// Usando GET
 echo '<h1>'.$_GET['nombre'].'</h1>';
 echo '<h1>'.$_GET['apellidos'].'</h1>';
 echo '<h1>'.$_GET['web'].'</h1>';
 
 // Todos los datos juntos
 var_dump($_GET);
+
+// Usando POST
+echo '<h1>'.$_POST['nombre'].'</h1>';
+echo '<h1>'.$_POST['apellidos'].'</h1>';
+echo '<h1>'.$_POST['web'].'</h1>';
+
+// Todos los datos juntos
+var_dump($_POST);
 
 ?>
