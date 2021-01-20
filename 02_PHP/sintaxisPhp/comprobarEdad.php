5cddde471f7fde6ee7ccce35df0c2fd182c580bd
Juan P. Romano <programming@jpromano.net>
2021-01-20 17:35:05 -0300

    Comprobar edad en PHP

--- 02_PHP/sintaxisPhp/comprobarEdad.php
+++ 02_PHP/sintaxisPhp/comprobarEdad.php
@@ -10,6 +10,6 @@ if($edad >= $mayoriaDeEdad){
     echo '<h1>'.$nombre.' es mayor de edad</h1>';
     echo '<h1> Y es de '.$ciudad.'</h1>';
 } else {
-    echo '<h2>'.$nombre.' es menor de edad</h2>';
+    echo '<h2>'.$nombre.' es menor de edad(tiene menos de 18)</h2>';
 }
 ?>
