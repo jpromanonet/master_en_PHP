83354c3592652367791ed30141792c00b357df78
Juan P. Romano <programming@jpromano.net>
2021-01-22 13:37:37 -0300

    Ejercicios switch

--- 02_PHP/sintaxisPhp/switch.php
+++ 02_PHP/sintaxisPhp/switch.php
@@ -2,4 +2,32 @@
 
 // Switch statement
 
+// Ejercicio 01
+$dia = 4;
+
+switch($dia){
+    case 1:
+        echo "Es Lunes";
+        break;
+    case 2:
+        echo "Es Martes";
+        break;
+    case 3:
+        echo "Es Miercoles";
+        break;
+    case 4:
+        echo "Es Jueves";
+        break;
+    case 5:
+        echo "Es Viernes";
+        break;
+    case 6:
+        echo "Es Sabado";
+        break;
+    case 7:
+        echo "Es Domingo";
+        break;
+
+}
+
 ?>
