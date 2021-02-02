235e05f08d697c879bbc3807d3fafb6a6176e7b5
jpromanonet <programming@jpromano.net>
2021-02-02 12:17:08 -0300

    Bucle while

--- 02_PHP/sintaxisPhp/bucleWhile.php
+++ 02_PHP/sintaxisPhp/bucleWhile.php
@@ -1,3 +1,15 @@
 <?php
 
+/*
+ * BUCLE WHILE
+ *
+ * Estructura de control que itera o repite la ejecucion de una serie de instrucciones
+ * tantas veces como sea necesario, en base a una condicion.
+ *
+ * while(condicion){
+ *      bloque de instrucciones
+ * }
+ *
+ * */
+
 ?>
