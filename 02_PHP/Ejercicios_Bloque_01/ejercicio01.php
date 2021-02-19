4abd84008a0659e62dedffafbb1bfc717aaf4d74
jpromanonet <programming@jpromano.net>
2021-02-19 16:50:35 -0300

    Ejercicio 01

--- 02_PHP/Ejercicios_Bloque_01/ejercicio01.php
+++ 02_PHP/Ejercicios_Bloque_01/ejercicio01.php
@@ -1,3 +1,14 @@
 <?php
 
+// Ejercicio 01
+
+// Crear 2 variables, pais y continente e imprimirlas en pantalla
+
+// Resolucion
+
+$pais = "Argentina";
+$continente = "America";
+echo $pais;
+echo $continente;
+
 ?>
