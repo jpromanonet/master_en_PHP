ee32615aeac76b4b7f6413282cdf0093f60b3e83
Juan P. Romano <programming@jpromano.net>
2021-04-20 12:06:59 -0300

    Ejercicio 02

--- 02_PHP/Ejercicios_Bloque_01/ejercicio02.php
+++ 02_PHP/Ejercicios_Bloque_01/ejercicio02.php
@@ -4,6 +4,10 @@
     - Escribir un script en PHP que nos muestre por pantalla, todos los numeros pares de hay del 1 al 100.    
 */
 
-
+for($i = 1; $i <= 100; $i++){
+    if($i%2){
+        echo $i." <br/>";
+    }
+}
 
 ?>
